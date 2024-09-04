701017334299c511b20b43da270d35b4184b4f27
Andrii <andriy.andrushko@gmail.com>
2024-09-04 15:57:02 -0500

    Stretch goals 2. Added abillity to enter values until ctrl + c is pressed

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -7,24 +7,34 @@ namespace Calculator
         static void Main(string[] args)
         {
             var serviceProvider = new ServiceCollection()
-            .AddTransient<ICalculator,CalculatorService>()
+            .AddTransient<ICalculator, CalculatorService>()
 
             .BuildServiceProvider();
 
+            var calculator = serviceProvider.GetRequiredService<ICalculator>();
             Console.WriteLine("Enter numbers separated by a comma (e.g., 1,2):");
-            string input = Console.ReadLine();
+            while (true)
+            {
+                try
+                {
+                    string input = Console.ReadLine();
 
-            try
-            {
-                var calculator = serviceProvider.GetRequiredService<ICalculator>();
-                (int, string) result = calculator.CalculateSum(input);
-                Console.WriteLine("The sum is: " + result.Item1);
-                Console.WriteLine("The formula is: " + result.Item2);
+                    // Check for Ctrl+C
+                    if (input == null)
+                    {
+                        break;
+                    }
+
+                    (int, string) result = calculator.CalculateSum(input);
+                    Console.WriteLine("The sum is: " + result.Item1);
+                    Console.WriteLine("The formula is: " + result.Item2);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                }
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Error: " + ex.Message);
-            }
+
         }
     }
 }
